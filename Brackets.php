fedefdd6edbf07f8226b92964ff674f747dfaae5
Gavamot <Gavamot@gmail.com>
2017-07-11 13:45:13 +0500

    edit
    edit

--- Brackets.php
+++ Brackets.php
@@ -4,20 +4,19 @@ class Brackets
 {
 	public function isBracketSequenceCorrect($str) 
 	{
-	$str = str_split($str);
-	$charts = ['{' => '}', '(' => ')','[' => ']' ];
-	$stack = [];
-	  
-	for($i = 0; $i < count($str); $i++)
-	{
-	  $c = $str[$i];
-	  if(array_key_exists($c, $charts))
-		  array_push($stack, $charts[$c]);
-	  if(in_array($c, $charts))
-		if (count($stack) === 0 || array_pop($stack) != $c) 
-			return false;
-	}
-	return count($stack) == 0;
+            $charts = ['{' => '}', '(' => ')','[' => ']' ];
+            $stack = [];
+
+            for($i = 0; $i < strlen($str); $i++)
+            {
+              $c = $str[$i];
+              if(array_key_exists($c, $charts))
+                      array_push($stack, $charts[$c]);
+              if(in_array($c, $charts))
+                    if (count($stack) === 0 || array_pop($stack) != $c) 
+                            return false;
+            }
+            return count($stack) == 0;
 	}
 }
 
