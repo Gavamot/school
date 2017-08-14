e3e138b293d32ef0a46493d7f492b0fb2bf524a4
Gavamot <Gavamot@gmail.com>
2017-08-14 14:47:00 +0500

    2

--- 2/Test/UserTest.php
+++ 2/Test/UserTest.php
@@ -5,6 +5,7 @@ namespace Test;
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../Model/User.php';
 
+
 class UserTest extends \PHPUnit\Framework\TestCase
 {
     protected $user;
@@ -21,13 +22,14 @@ class UserTest extends \PHPUnit\Framework\TestCase
     // ------------- Password -------------
     public function testSetPasswordShort()
     {
-        $this->expectException(InvalidArgumentException::class);
+
+        $this->expectException(\InvalidArgumentException::class);
         $this->user->setPassword("12345");
     }
 
     public function testSetPasswordLong()
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(\InvalidArgumentException::class);
         $this->user->setPassword("1234567890123");
     }
 
@@ -40,20 +42,21 @@ class UserTest extends \PHPUnit\Framework\TestCase
     public function testSetCorrectPasswordHash()
     {
         $this->user->setPassword("123456");
-        $this->assertEquals(md5("123456"), $this->user->getPasswordHash());
+        $this->assertEquals(password_verify("123456", $this->user->getPasswordHash()), true);
+        //$this->assertEquals(password_hash($pwd, PASSWORD_DEFAULT), $this->user->getPasswordHash());
     }
 
     // ------------- Name -------------
 
     public function testSetNameShort()
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(\InvalidArgumentException::class);
         $this->user->setName("Va");
     }
 
     public function testSetNameLong()
     {
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(\InvalidArgumentException::class);
         $this->user->setName("Vasia Petrenkovish");
     }
 
